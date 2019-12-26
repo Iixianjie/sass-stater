96609344c310ada2a6852476fb326b55207eea95
lxj <1073830130@qq.com>
2019-12-27 00:13:29 +0800

    feat: 添加ts和js版的变量

---
+++ _base/util.js
@@ -0,0 +1,27 @@
+export function ellipsisX(rowNum = 1) {
+    if (rowNum !== 1) {
+        return `
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: ${rowNum};
+      -webkit-box-orient: vertical;
+    `;
+    }
+    return `
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  `;
+}
+export function size(size) {
+    return `
+  width: ${size};
+  height: ${size};
+  `;
+}
+export function wh(w, h) {
+    return `
+  width: ${w};
+  height: ${h};
+  `;
+}
